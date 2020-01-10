84bfe1ed47124c4071842d928b0d6cba63a59511
qing3gan <qing3gan@gmail.com>
2020-01-10 08:03:15 +0800

    thymeleaf.

--- src/main/java/com/agony/controller/BookController.java
+++ src/main/java/com/agony/controller/BookController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * desc
@@ -25,5 +26,13 @@ public class BookController {
     @GetMapping("/book")
     public String book() {
         return book.toString() + "\n" + books.toString();
+    }
+
+    @GetMapping("/books")
+    public ModelAndView books() {
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("books", books.getBooks());
+        mv.setViewName("books");
+        return mv;
     }
 }
