3506c84a5f03a037caa1438ce87ca858b166798f
qing3gan <qing3gan@gmail.com>
2020-01-13 22:59:29 +0800

    freemarker.

--- src/main/java/com/agony/controller/BookController.java
+++ src/main/java/com/agony/controller/BookController.java
@@ -5,6 +5,7 @@ import com.agony.bean.Books;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -28,11 +29,11 @@ public class BookController {
         return book.toString() + "\n" + books.toString();
     }
 
-    @GetMapping("/books")
-    public ModelAndView books() {
+    @GetMapping("/books/{template}")
+    public ModelAndView books(@PathVariable String template) {
         ModelAndView mv = new ModelAndView();
         mv.addObject("books", books.getBooks());
-        mv.setViewName("books");
+        mv.setViewName("books-" + template);
         return mv;
     }
 }
